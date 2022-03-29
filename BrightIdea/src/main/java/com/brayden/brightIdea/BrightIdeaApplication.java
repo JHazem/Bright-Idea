5e4f20c04c987a16159d6a4f1c8a8c754c431437
Brayden Rachel <86629850+BraydenRachel@users.noreply.github.com>
2022-03-29 14:07:01 -0500

    Add files via upload

---
+++ BrightIdea/src/main/java/com/brayden/brightIdea/BrightIdeaApplication.java
@@ -0,0 +1,13 @@
+package com.brayden.brightIdea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrightIdeaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrightIdeaApplication.class, args);
+	}
+
+}
